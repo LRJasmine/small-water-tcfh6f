6ec42d49fa9b286352b376e9628e15767da069c7
CodeSandbox <support@codesandbox.io>
2023-09-26 20:28:27 +0000

    Sandbox snapshot commit

--- learnphp.php
+++ learnphp.php
@@ -7,3 +7,14 @@ foreach ($fruits as $fruit) {
 echo ('<pre>');
 print_r($fruits);
 echo ('</pre>');
+$x = 2;
+echo($x*=3);
+
+$y = 'Happy Days';
+$z = "That's awesome";
+$results = $y===1 ? "Oh no!" : $z;
+echo($results);
+
+$andre = null;
+$bab = $andre ?? "Andre is nulls";
+echo($bab);
