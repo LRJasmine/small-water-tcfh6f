770efe0f7cf00a397deaa0d4927390de090b89f5
LRJasmine <55485606+LRJasmine@users.noreply.github.com>
2023-12-26 21:41:19 +0000

    Update App.php and index.php

--- learningphpprojects/branch1.31/app/App.php
+++ learningphpprojects/branch1.31/app/App.php
@@ -2,4 +2,62 @@
 
 declare(strict_types = 1);
 
+echo '<br> Start of App.php';
+
+function getAllTransactionFiles(string $dirPath): array{
+    $files = [];
+
+    foreach(scandir($dirPath) as $file){
+        if(is_dir($file)){
+            continue;
+        }
+
+        $files[] = $dirPath . $file;
+    }
+
+    return $files;
+}
+
+function getAllTransactions(string $fileName, ?callable $transactionHandler = null): array {
+    if (!file_exists($fileName)){
+        trigger_error('File"' . $fileName . '" does not exist.', E_USER_ERROR);
+    }
+     echo '<br> fileName is: '. $fileName . '<br>';
+
+     $file = fopen($fileName, 'r');
+
+     fgetcsv($file);
+
+     $transactions = [];
+
+     while(($transactions = fgetcsv($file)) !== false){
+        if($transactionHandler !== null){
+            $transaction = $transactionHandler($transactions);
+        }
+
+        echo '<br> -------';
+        echo implode($transaction);
+        echo '<br> -------';
+
+        $transactions[] = $transaction;
+     }
+
+     return $transactions;
+}
+
+function extractTransaction(array $transactionRow): array{
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$',','], '', $amount);
+
+    return [
+        'date' => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount' => $amount,
+    ];
+}
+
+echo '<br> End of App.php';
+
 // Your Code
--- learningphpprojects/branch1.31/public/index.php
+++ learningphpprojects/branch1.31/public/index.php
@@ -8,4 +8,24 @@ define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
+echo '<br> Start of Index.php';
+require APP_PATH . 'App.php';
+
+$files = getAllTransactionFiles(FILES_PATH);
+
+echo '<br> ------------------- <br>';
+print_r('These are the files');
+print_r($files);
+echo '<br> ------------------- <br>';
+
+$transactions = [];
+foreach($files as $file){
+    $transactions = array_merge($transactions, getAllTransactions($file, 'extractTransaction'));
+}
+echo '<br> ------------------- <br>';
+print_r('These are the transactions');
+print_r($transactions);
+echo '<br> ------------------- <br>';
+
+echo '<br> End of Index.php';
 /* YOUR CODE (Instructions in README.md) */
